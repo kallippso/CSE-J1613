01b960bdda91ac6ab4c3cdbf8cd9aebd6692dc8d
JohnTouya <JohnNgo94@charter.net>
2016-04-14 21:50:59 -0500

    Shop and Order update
    Shop class has been updated with ArrayLists for its data, along with
    createPart, createModel, newCustomer, newSalesAssoc, placeOrder methods
    from previous sprints.
    The Shop and Order classes have been updated to add the viewOrder
    functionality.
    Small changes to various other classes to improve compatibility.

--- RobbieRobotShop/src/Shop/Shop.java
+++ RobbieRobotShop/src/Shop/Shop.java
@@ -5,26 +5,44 @@
  */
 package Shop;
 import java.awt.image.RenderedImage;
+import java.util.ArrayList;
 
 /**
- *
- * @author Luis
+ * Description: For Group J1613's Robot Project - the class defining a Shop
  */
+
 public class Shop {
 
-    public static void main(String[] args) {
-
-
-    }
-
-
+    //private instance variables
+    private ArrayList<RobotModel> robotModels;
+    private ArrayList<Order> orders;
+    private ArrayList<Customer> customers;
+    private ArrayList<SalesAssoc> salesAssoc;
+    private ArrayList<Head> headList;
+    private ArrayList<Locomotor> locomotorList;
+    private ArrayList<Torso> torsoList;
+    private ArrayList<Battery> batteryList;
+    private ArrayList<Arm> armList;
+    
+    // constructors
     public Shop()
     {
-
-
+        robotModels = new ArrayList<>();
+        orders = new ArrayList<>();
+        customers = new ArrayList<>();
+        salesAssoc = new ArrayList<>();
+        headList = new ArrayList<>();
+        locomotorList = new ArrayList<>();
+        torsoList = new ArrayList<>();
+        batteryList = new ArrayList<>();
+        armList = new ArrayList<>();
     }
-
-
+    
+    // methods
+        // getters
+    //public void getPart(ComponentType type, int search)
+    
+        // createPart methods
     public void createPart(ComponentType componentName, String name, double weight, double cost, String description, RenderedImage image)
     {
        RobotPart newModel;
@@ -52,36 +70,118 @@ public class Shop {
                break;
            default:
                System.out.println("Component name not recognized.");
-               break;
-               
+               break;        
        }
     }
-
-
-    void newCustomer()
+    
+    public void createPart(String name, int num, String type, double weight, double cost, String description)    // only heads and arms fit this constructor
     {
+       switch(type)
+       {
+           case "head": //add to shop's data structure
+               headList.add(new Head(name, num, weight, cost, description));
+               break;
+           case "arm"://add to shop's data structure
+               armList.add(new Arm(name, num, weight, cost, description));
+               break;
+           default:
+               System.out.println("Component name not recognized.");
+               break;        
+       }
+    }
+    
+    public void createPart(String name, int num, String type, double weight, double cost, String description, int value) // only torsos and locomotors fit this constructor
+    {
+       switch(type)
+       {
+           case "torso": //add to shop's data structure
+               torsoList.add(new Torso(name, num, type, weight, cost, description, value));
+               break;
+           case "locomotor"://add to shop's data structure
+               locomotorList.add(new Locomotor(name, num, type, weight, cost, description, value));
+               break;
+           default:
+               System.out.println("Component name not recognized.");
+               break;        
+       }
+    }
+    
+    public void createPart(String name, int num, String type, double weight, double cost, String description, double energy, double power)   // only batteries fit this constructor
+    {
+        switch(type)
+       {
+           case "battery": //add to shop's data structure
+               batteryList.add(new Battery(name, num, type, weight, cost, description, energy, power));;
+               break;
+           default:
+               System.out.println("Component name not recognized.");
+               break;        
+       }
+    }
+    
+        // create RobotModel methods
+    public void createRobotModel(Head head, Locomotor motor, Torso body, Battery b1, Arm arm1)
+    {
+        robotModels.add(new RobotModel(head, motor, body, b1, arm1));
+    }
+    public void createRobotModel(Head head, Locomotor motor, Torso body, Battery b1, Arm arm1, Arm arm2)
+    {
+        robotModels.add(new RobotModel(head, motor, body, b1, arm1, arm2));
+    }
+    public void createRobotModel(Head head, Locomotor motor, Torso body, Battery b1, Battery b2, Arm arm1)
+    {
+        robotModels.add(new RobotModel(head, motor, body, b1, b2, arm1));
+    }
+    public void createRobotModel(Head head, Locomotor motor, Torso body, Battery b1, Battery b2, Arm arm1, Arm arm2)
+    {
+        robotModels.add(new RobotModel(head, motor, body, b1, b2, arm1, arm2));
+    }
+    public void createRobotModel(Head head, Locomotor motor, Torso body, Battery b1, Battery b2, Battery b3, Arm arm1)
+    {
+        robotModels.add(new RobotModel(head, motor, body, b1, b2, b3, arm1));
+    }
+    public void createRobotModel(Head head, Locomotor motor, Torso body, Battery b1,Battery b2, Battery b3, Arm arm1, Arm arm2)
+    {
+        robotModels.add(new RobotModel(head, motor, body, b1, b2, b3, arm1, arm2));
+    }
 
+        // other methods
+    public void newCustomer(String name, int num)
+    {
+        customers.add(new Customer(name, num));
+    }
+    
+    public void newSalesAssoc(String name, int num)
+    {
+        salesAssoc.add(new SalesAssoc(name, num));
+    }
+    
+    public void placeOrder(int num, Date date, Customer cust, SalesAssoc sales, RobotModel model, Status status)
+    {   
+        orders.add(new Order(num, date, cust, sales, model, status));
+    }
+    
+    public void viewOrders()
+    {
+        int i = 0;
+        while(orders.get(i) != null)
+        {
+            System.out.println(orders.get(i));
+            i++;
+        }
+    }
+    
+    public void runReport()
+    {
 
     }
 
-    void placeOrder()
+    public void save()
     {
 
+    }
+     
+    public static void main(String[] args) {
 
     }
-
-
-    void runReport()
-    {
-
-
-    }
-
-
-    void save()
-    {
-
-
-    }
-
 }
